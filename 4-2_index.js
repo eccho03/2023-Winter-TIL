533df69bb259b201979b81fe50c2a326faff4347
eccho03 <choeunchae0828@gmail.com>
2024-01-08 21:57:35 +0900

    study day 3

---
+++ 4-2_index.js
@@ -0,0 +1,8 @@
+const calc = require("./4-2_calc");
+
+console.log(calc.add(1, 2));
+console.log(calc.add(4, 5));
+console.log(calc.sub(10, 2));
+
+// require은 nodejs의 내장함수이기 때문에, 기존의 vanilla js에서는 사용이 제한됨
+

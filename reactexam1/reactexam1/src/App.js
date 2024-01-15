cd8eb46d04e11d8c739dd70a573e10985c97e18e
eccho03 <choeunchae0828@gmail.com>
2024-01-15 21:12:52 +0900

    study day2 2 - create react app

---
+++ reactexam1/reactexam1/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+
+  let name = "조은채";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>안녕 리액트 {name} </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
